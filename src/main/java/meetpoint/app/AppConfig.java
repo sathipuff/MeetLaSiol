748688ca1337c63cd0c4643a5cfcab564fbb93aa
potato <rachelwyl92@gmail.com>
2018-02-10 22:41:18 +0800

    add mongodb connection

--- src/main/java/meetpoint/app/AppConfig.java
+++ src/main/java/meetpoint/app/AppConfig.java
@@ -1,9 +1,13 @@
 package meetpoint.app;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 @Configuration
 public class AppConfig {
@@ -12,13 +16,31 @@ public class AppConfig {
      * Use the standard Mongo driver API to create a com.mongodb.MongoClient instance.
      */
     public @Bean
-    MongoClient mongoClient() {
-        return new MongoClient("localhost");
-        /*return new MongoClient(singletonList(new ServerAddress("127.0.0.1", 27017)),
-      singletonList(MongoCredential.createCredential("name", "db", "pwd".toCharArray())));*/
+    MongoClient mongoClient()  {
+        //return new MongoClient("localhost");
+
+        MongoClientURI uri = new MongoClientURI("mongodb://localhost");
+
+        try {
+            String password = URLEncoder.encode("MagicalMe@1318", "UTF-8");
+
+
+            uri = new MongoClientURI(
+                "mongodb://admin:"+password+"@cluster0-shard-00-00-ooctf.mongodb.net:27017," +
+                        "cluster0-shard-00-01-ooctf.mongodb.net:27017" +
+                        ",cluster0-shard-00-02-ooctf.mongodb.net:27017" +
+                        "/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+
+        } finally
+        {
+            return new MongoClient(uri);
+        }
     }
 
     public @Bean MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "mydatabase");
+        return new MongoTemplate(mongoClient(), "mpDatabase");
     }
 }
--- src/main/java/meetpoint/controllers/GreetingController.java
+++ src/main/java/meetpoint/controllers/GreetingController.java
@@ -1,8 +1,13 @@
 package meetpoint.controllers;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import meetpoint.model.Greeting;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +18,32 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    MongoOperations mongoOps;
+
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
+
+        Greeting greeting = new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
+
+        mongoOps.insert(greeting);
+
+        return greeting;
+    }
+
+    @RequestMapping("/pastGreetings")
+    public String getGreetings()
+    {
+        List<Greeting> greetingList =  mongoOps.findAll(Greeting.class);
+
+        String pastGreetings = "Size of greetings: " + greetingList.size() + ", list = ";
+
+        for (Greeting greeting : greetingList)
+        {
+            pastGreetings = pastGreetings.concat("" + greeting.getContent() + ",");
+        }
+
+        return pastGreetings;
     }
 }
